728804f1fc1c1fc448273ea3162a311afccf86c7
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2021-04-11 00:02:58 +0800

    WIP: working on printing the right amount of stars.

--- diamond.rb
+++ diamond.rb
@@ -4,20 +4,31 @@ require "./diamond.rb"
 Diamond.new.main if __FILE__ == $PROGRAM_NAME
 
 class Diamond
-  
-  SPACE = "-"
-
   def main
     puts "Running Main.new.main..."
-    exp1(level=5, border=4)
+    exp1(5, 2)
   end
   
   def exp1(level, border)
+    no_of_stars = 1
+
     #pad the space
     (level -1).downto 1 do |no_of_spaces|
-      no_of_spaces.times { print SPACE }
-      puts "x"  
+      no_of_spaces.times { print "-" }
+      puts "x#{no_of_stars}"
+      
+      no_of_stars = no_of_stars + 2  
     end  
   end
 
+=begin
+      for i in 1..(level * 2) - 2 do
+        if i.odd?
+          i.times { print "*"}
+        end
+        puts
+      end
+
+=end
+  
 end
