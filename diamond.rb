a0c0d4ff318d3d3f768fb8e6b5a9c5b56d0c0a02
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2021-04-10 23:06:26 +0800

    WIP: define the code that print that 'pads' spaces for each level.

--- diamond.rb
+++ diamond.rb
@@ -7,6 +7,17 @@ class Diamond
 
   def main
     puts "Running Main.new.main..."
+    exp1(level=5, border=4)
+  end
+  
+  def exp1(level, border)
+    #pad the space
+    (level -1).downto 1 do |i|
+      i.times do
+        print "-"
+      end
+      puts "x"  
+    end  
   end
 
 end
