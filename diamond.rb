624bb07cf7c85d12585987ebdc52413bb97f787b
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2021-04-11 00:35:30 +0800

    WIP: added the logic where to start implementing the borders.

--- diamond.rb
+++ diamond.rb
@@ -6,22 +6,30 @@ Diamond.new.main if __FILE__ == $PROGRAM_NAME
 class Diamond
   def main
     puts "Running Main.new.main..."
-    exp1(2, 2)
+    exp1(6, 2)
   end
   
   def exp1(level, border)
+    level_count = 1
     no_of_stars = 1
 
     #pad the space
     (level -1).downto 1 do |no_of_spaces|
       no_of_spaces.times { print "-" }
 
-      # print the no. of stars
-      no_of_stars.times do
-        print "*"
-      end  
+      if level_count < border + 1
+        no_of_stars.times do
+          print "*"
+        end
+      else
+        no_of_stars.times do
+          print "x"
+        end  
+      end   
+       
       no_of_stars = no_of_stars + 2  
-      puts 
+      puts " #{level_count}"
+      level_count = level_count + 1
     end  
   end
 
